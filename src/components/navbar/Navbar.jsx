fb61615e24fdfbc05d5a9e4b1e93e1274367dac1
Emily Dawson <lemilydawson@gmail.com>
2023-05-08 16:28:58 +0100

    navbar.css styles added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+import './navbar.css'
+
 import './navbar.css'
  const Navbar = () => {
   return (
-    <div>
-        Navbar
+    <div className='company__navbar'>
+        <div className='company__navbar-links'>
+          <div className='company__navbar-links_logo'>
+            <img src={logo} alt="logo"/>
+          </div>
+        </div>
     </div>
   )
 }
