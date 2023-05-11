26897e8e6c5559525c597f30457f4107f152aa0c
Emily Dawson <lemilydawson@gmail.com>
2023-05-11 13:07:07 +0100

    [feature_01]_add-article-component

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './blog.css'
 import {Article} from '../../components'
+import {blog01, blog02, blog03, blog04, blog05} from './imports.js'
  const Blog = () => {
   return (
     <div className='company__blog section__padding' id="blog">
@@ -9,14 +10,14 @@ import {Article} from '../../components'
         </div>
         <div className='company__blog-container'>
           <div className='company__blog-container_groupA'>
-            <Article/>
+            <Article imgUrl={blog01} date="the First Age" title="The Fellowship of the Ring"/>
           </div>
           <div className='company__blog-container'>
           <div className='company__blog-container_groupB'>
-            <Article/>
-            <Article/>
-            <Article/>
-            <Article/>
+            <Article imgUrl={blog02} date="the First Age" title="The Fellowship of the Ring"/>
+            <Article imgUrl={blog03} date="the First Age" title="The Fellowship of the Ring"/>
+            <Article imgUrl={blog04} date="the First Age" title="The Fellowship of the Ring"/>
+            <Article imgUrl={blog05} date="the First Age" title="The Fellowship of the Ring"/>
           </div>
         </div>
       </div>
