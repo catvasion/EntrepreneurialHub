74ef6ea518afafd46867b83acae8d2c6336758d5
Emily Dawson <lemilydawson@gmail.com>
2023-05-11 11:40:55 +0100

    [feature_01 Add cta component]" "

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import './cta.css'
  const CTA = () => {
   return (
-    <div>
-        CTA
+    <div className='company__cta'>
+        <div className='company__cta-content'>
+          <p>Request your own staff!</p>
+          <h3>Register today and get a special Gandalf discount</h3>
+        </div>
+        <div className='company__cta-button'>
+          <button type='button'>Get Started!</button>        
+        </div>
     </div>
   )
 }
