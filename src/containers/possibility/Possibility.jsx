4c1dbc296ff89a2d8e9e99d00b426e63cf92e1b6
Emily Dawson <lemilydawson@gmail.com>
2023-05-11 11:32:40 +0100

    [feature_01 Add posibility styling + responsive styling]

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,13 +3,13 @@ import './possibility.css'
 import tree from '../../assets/gondor-tree.png'
  const Possibility = () => {
   return (
-    <div className='container__possibility section__padding' id='possibility'>
+    <div className='company__possibility section__padding' id='possibility'>
       <div className='company__possibility-image'>
         <img src={tree} alt='tree of gondor'/>
       </div> 
-      <div company__possibilty-content>
+      <div className='company__possibility-content'>
         <h4> Gondor survived to the end of the Third Age</h4>
-        <h1 className='gradient-text'>The greatest realm of Men in the west of Middle-earth.</h1>
+        <h1 className='gradient__text'>The greatest realm of Men in the west of Middle-earth.</h1>
         <p>The city of kings!</p>
         <h4>Request a letter from Gondor.</h4>
       </div>
