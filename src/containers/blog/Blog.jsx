5c733d54146dbc9819de5fde26fdcf0502849171
Emily Dawson <lemilydawson@gmail.com>
2023-05-11 12:35:02 +0100

    [feature_01]_add-blog-component

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './blog.css'
+import {Article} from '../../components'
  const Blog = () => {
   return (
-    <div>
-        Blog
+    <div className='company__blog section__padding' id="blog">
+        <div className='company__blog-heading'>
+          <h1 className='gradient__text'>Curious about the hobbits? Gandalf does like to blog!</h1>
+        </div>
+        <div className='company__blog-container'>
+          <div className='company__blog-container_groupA'>
+            <Article/>
+          </div>
+          <div className='company__blog-container'>
+          <div className='company__blog-container_groupB'>
+            <Article/>
+            <Article/>
+            <Article/>
+            <Article/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
